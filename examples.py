194ddd32d159743f5a5f305cd0cb477ba3b69224
Theo Carr <theo@Theos-MacBook-Pro.local>
2021-06-25 10:42:14 -0400

    added to examples

--- examples.py
+++ examples.py
@@ -1,7 +1,12 @@
+
 from plotting import plot_setup
 import matplotlib.pyplot as plt
 import xarray as xr
 import numpy as np
 import cartopy.crs as ccrs
+import cmocean
 
+sst = xr.open_dataarray('sst_may2021.nc')
 
+fig,ax = plot_setup(plot_range=[0,359,-40,40], central_lon=180)
+ax.plot(sst.longitude, sst.latitude, sst.mean('time')
