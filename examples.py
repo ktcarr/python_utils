484ccd22bd21893616dc2e559bb6dc7ff2f9b097
Theo Carr <theo@Theos-MacBook-Pro.local>
2021-07-08 11:52:43 -0400

    added orthographic projection, wind vector plots, and animation example

--- examples.py
+++ examples.py
@@ -1,17 +1,29 @@
-from plotting import plot_setup
+'''Note: this file is no longer updated. Please see examples.ipynb for new version.'''
+
+from plotting import plot_setup_platecarree, plot_setup_orthographic
 import matplotlib.pyplot as plt
 import xarray as xr
 import numpy as np
 import cartopy.crs as ccrs
 import cmocean
+from os.path import join
 
-sst = xr.open_dataarray('sst_may2021.nc')
+data_folder = '~/data'
+sst = xr.open_dataarray(join(data_folder,'sst_may2021.nc'))
 
-fig,ax = plot_setup(plot_range=[0,359,-40,40], central_lon=180,figsize=(8,4))
+# Plot of global SST using PlateCarree projection
+fig,ax = plot_setup_platecarree(plot_range=[0,359,-40,40], central_lon=180,figsize=(8,4))
 cp     = ax.contourf(sst.lon, sst.lat, sst.mean('time'), cmap='cmo.thermal',
                      levels=np.arange(9,36,3), extend='both', 
                      transform=ccrs.PlateCarree())
-cb     = fig.colorbar(cp, orientation='horizontal', label=r'SST$^{\circ}$C',
-                      pad=.05)
+cb     = fig.colorbar(cp, orientation='horizontal', label=r'SST$^{\circ}$C',pad=.05)
 ax.set_title('Mean SST for May 2021')
+plt.show()
+
+
+# Plot of North Atlantic SST using Orthographic projection
+fig,ax = plot_setup_orthographic(figsize=(8,7))
+cp     = ax.contourf(sst.lon, sst.lat, sst.mean('time'), cmap='cmo.thermal',
+                     levels=np.arange(0,36,3), extend='both', transform=ccrs.PlateCarree())
+cb     = fig.colorbar(cp, orientation='horizontal', label=r'SST$^{\circ}$C', pad=.05)
 plt.show()
--- plotting.py
+++ plotting.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import cartopy.feature as cfeature
 import cartopy.crs as ccrs
 
-def plot_setup(plot_range = [-125.25,-66,22.5,50], figsize = (7,5), central_lon=0, state_borders=True):
+def plot_setup_platecarree(plot_range = [-125.25,-66,22.5,50], figsize = (7,5), central_lon=0, state_borders=True):
     '''Function sets up plotting environment for continental US. 
 	"plot_range" is geographical extent, in format [lon1, lon2, lat1, lat2]
         "figsize" controls size of figure.
@@ -11,17 +11,26 @@ def plot_setup(plot_range = [-125.25,-66,22.5,50], figsize = (7,5), central_lon=
        The function returns fig and ax objects'''
      
     fig = plt.figure(figsize = figsize)                                       # Set up figure for plotting
-    ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=central_lon))
-    states_provinces = cfeature.NaturalEarthFeature(                          # get state outlines
-        category='cultural',
-        name='admin_1_states_provinces_lines',
-        scale='50m',
-        facecolor='none')
-    if state_borders:
-        ax.add_feature(states_provinces, edgecolor='black')
+    ax = plt.axes(projection=ccrs.PlateCarree(central_longitude=central_lon)) # Use map background
     ax.coastlines()                                                           # draw coastlines
     ax.set_extent(plot_range, crs=ccrs.PlateCarree())                         # limit geographic extent of plot
     ax.add_feature(cfeature.BORDERS)                                          # draw country borders
     ax.title.set_fontsize(30)
-    
+    if state_borders:
+        ax.add_feature(cfeature.NaturalEarthFeature(category  ='cultural',    # Get state outlines
+                                                    name      ='admin_1_states_provinces_lines',
+                                                    scale     ='50m',
+                                                    facecolor ='none'),
+                       edgecolor='black')
     return fig, ax
+
+def plot_setup_orthographic(plot_range=[-100,20,0,60], central_lon=-40, central_lat=30, figsize=(7,5)):
+    '''Set up figure for plotting an orthographic projection (curved globe).
+    "central_lon" and "central_lat" specify where to center the projection. Function returns fig,ax'''
+    fig = plt.figure(figsize = figsize)                                       
+    ax = plt.axes(projection=ccrs.Orthographic(central_longitude=central_lon, 
+                                               central_latitude=central_lat)) # Use map background
+    ax.coastlines()                                                           # draw coastlines
+    ax.add_feature(cfeature.BORDERS)                                          # draw country borders
+    ax.set_extent(plot_range, crs=ccrs.PlateCarree())
+    return fig,ax
